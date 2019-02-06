8e280f8981ee62d60e2967d9d810e8ce3b663d11
Baptiste Revel <21700919@campus.unicaen.fr>
2019-02-06 16:09:35 +0100

    Liste fonctionnelle

--- TD2/app/routes/ex2.js
+++ TD2/app/routes/ex2.js
@@ -1,11 +1,46 @@
 import Route from '@ember/routing/route';
+import EmberObject,{computed} from '@ember/object';
 
-const Services=EmberObject.extend() {
+let Services=EmberObject.extend({
   services:[],
   countActive:computed('services.@each.active',function(){
-
+    let activeServices=this.get('services').filter(
+      service=>service.active
+    );
+    return activeServices.length;
+  }),
+  sumActive: computed('services.@each.active',function(){
+    let total = 0;
+    this.get('services').forEach(service=>{
+      if(service.active)
+        total += service.price;
+    });
+    return total;
   })
-}
+});
 
 export default Route.extend({
+  model(){
+    return Services.create({services:
+        [
+          {
+            "name": "Web Development",
+            "price": 300,
+            "active":true
+          },{
+          "name": "Design",
+          "price": 400,
+          "active":false
+        },{
+          "name": "Integration",
+          "price": 250,
+          "active":false
+        },{
+          "name": "Formation",
+          "price": 220,
+          "active":false
+        }
+        ]
+      });
+  }
 });
