d965ac1555c047c20b7b8e8e06dabe196c738e10
Baptiste Revel <21700919@campus.unicaen.fr>
2019-02-12 17:37:52 +0100

    Multi select eléments dispo fonctionel

---
+++ TD2/app/helpers/array-contains.js
@@ -0,0 +1,9 @@
+
+import Ember from 'ember';
+
+export function arrayContains(params) {
+  const [items, value] = params;
+  return items.includes(value);
+}
+
+export default Ember.Helper.helper(arrayContains)
